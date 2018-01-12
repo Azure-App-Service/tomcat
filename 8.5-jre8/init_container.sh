67efb8bc01c090bfc42bd2e8f64cfb2e817db1ff
Shrirang Shirodkar <shrirs@microsoft.com>
2018-01-11 23:19:34 -0800

    Allow custom Tomcat configurations

---
+++ 8.5-jre8/init_container.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+cat >/etc/motd <<EOL 
+  _____                               
+  /  _  \ __________ _________   ____  
+ /  /_\  \\___   /  |  \_  __ \_/ __ \ 
+/    |    \/    /|  |  /|  | \/\  ___/ 
+\____|__  /_____ \____/ |__|    \___  >
+        \/      \/                  \/ 
+A P P   S E R V I C E   O N   L I N U X
+
+Documentation: http://aka.ms/webapp-linux
+EOL
+cat /etc/motd
+
+echo Printing list of environment variables
+printenv
+echo Done printing environment variables
+
+echo Starting ssh service
+service ssh start
+
+# If a custom initialization script is defined, run it and exit.
+if ! [ -z $INIT_SCRIPT ]
+then
+    echo Running custom initialization script
+    source $INIT_SCRIPT
+    echo Finished running custom initialization script. Exiting.
+    exit
+fi
+
+# Default to CATALINA_BASE=/home/tomcat
+if [[ -z $CATALINA_BASE && -a /home/tomcat/conf/server.xml ]]
+then
+    export CATALINA_BASE=/home/tomcat
+fi
+
+# if IGNORE_CATALINA_BASE=1 or true, unset CATALINA_BASE
+if [[ "$IGNORE_CATALINA_BASE" = "1" ||  "$IGNORE_CATALINA_BASE" = "true" ]]
+then
+    echo Setting CATALINA_BASE to empty as IGNORE_CATALINA_BASE is set to $IGNORE_CATALINA_BASE
+    export CATALINA_BASE=
+fi
+
+# Start Tomcat
+echo Starting Tomcat with CATALINA_BASE set to \"$CATALINA_BASE\"
+catalina.sh run
+
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-"# tomcat" 
+# Apache Tomcat on Azure App Service
+
+This repository contains Docker images for Apache Tomcat for running on Azure App Service.
+
+## Build
+```
+cd 8.5-jre8
+docker build -t tomcat .
+```
+
+## Run
+
+```
+docker run tomcat
+```
+
+## SSH to the Docker container
+
+```
+container_ip=IP_OF_THE_DOCKER_CONTAINER
+ssh root@$container_ip -p 2222
+```
+
+The username is `root` and password is `Docker!`. For detailed documentation, refer https://docs.microsoft.com/en-us/azure/app-service/containers/tutorial-custom-docker-image.
+
