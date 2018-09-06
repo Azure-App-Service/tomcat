04bb5d281e7959daf64727723143fdef8ce8b8c7
shrishrirang <shrirs@microsoft.com>
2018-09-06 16:03:01 -0700

    Change base image from docker-library/tomcat to appsvc/java
    - appsvc/java contains the official zulu bits for Azure. This commit adds Dockerfiles in directories 8.5-jre8 and 9.0-jre8 to use appsvc/java as the base image which in turn uses Ubuntu as the base image.
    - The alpine directories continue to use docker-library/tomcat as the base image which in turn uses openjdk and alpine as the base images.
    - Now that we don't use docker-library/tomcat as the base image, we need to add tomcat bits to the docker image ourselves. This commit contains those changes.
    - Moved a few lines around in the Dockerfiles to make sure directories are created before they are used and they aren't used before they are created, etc
    - During development, if you need to build the image using a base image other than appsvc/java, you can pass it as a parameter to the 'docker build' command. Refer README for details.

--- setup.sh
+++ setup.sh
@@ -9,3 +9,7 @@ cp -r shared 8.5-jre8-alpine/tmp/shared
 rm -rf 9.0-jre8-alpine/tmp/shared
 mkdir -p 9.0-jre8-alpine/tmp
 cp -r shared 9.0-jre8-alpine/tmp/shared
+
+rm -rf 9.0-jre8/tmp/shared
+mkdir -p 9.0-jre8/tmp
+cp -r shared 9.0-jre8/tmp/shared
