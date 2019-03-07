41aa8b786f097ee0215b3f2d6ca6262961f16ddc
pagariyaalok <alok.pagariya@microsoft.com>
2019-03-07 15:42:30 -0800

    Changes added related to AppService filter and few comments added.

--- 8.5-java11/init_container.sh
+++ 8.5-java11/init_container.sh
@@ -44,15 +44,17 @@ then
 fi
 
 # WEBSITE_INSTANCE_ID will be defined uniquely for each worker instance while running in Azure.
-# During development it may not be defined, in that case  we set WEBSITE_INSTNACE_ID=dev.
+# During DevDelopment, define environment variables required for testing.
 if [ -z "$WEBSITE_INSTANCE_ID" ]
 then
     export WEBSITE_INSTANCE_ID=dev
-    # set the Env variables required to test AzMon
+    
+    # BEGIN: Set the Env variables required to test AzMon
     export HTTP_LOGGING_ENABLED=1
     export WEBSITE_HOSTNAME=dev.appservice.com
     export APPSETTING_WEBSITE_AZMON_ENABLED=True
     export DIAGNOSTIC_LOGS_MOUNT_PATH=/var/log/diagnosticLogs
+    # END: Set the Env variables required to test AzMon
 fi
 
 # BEGIN: Configure App Insights
