6f1b63d45057a5d94cc415398a7c0ff070cf4ef5
Shrirang Shirodkar <shrirs@microsoft.com>
2019-03-07 17:04:42 -0800

    Remove duplication
    - This commit adds 3 directories:
       shared\tomcat\common - this contains files common to all Tomcat versions
       shared\tomcat\8.5 - this contains files specific to Tomcat 8.5
       shared\tomcat\9.0 - this contains files specific to Tomcat 9.0
    - Files from individual Tomcat version directories have been deleted and are now moved to one of the above 3 directories, as appropriate
    - shared\tomcat\common\Dockerfile is a template Dockerfile which is processed by setup.ps1 to generate a version specific Dockerfile

--- scripts/setup.ps1
+++ scripts/setup.ps1
@@ -1,15 +1,70 @@
 function setup
 {
 	param([string]$version)
-   
+
+    # Remove tmp directory
 	$dirpath = $version + '/tmp/shared'
-	
 	If (Test-Path $dirpath)
 	{
 		remove-item -recurse -force $dirpath
 	}
-	
-	copy-item -recurse shared $dirpath
+
+    # Copy the shared files to the target directory
+    copy-item -recurse shared $dirpath
+    copy-item -recurse shared\tomcat\common\* $version
+
+    $dockerFileTemplatePath = '.\shared\tomcat\common\Dockerfile'
+    $dockerFileOutPath = "$version\Dockerfile"
+
+    # Generate the Dockerfile from the template and place it in the target directory
+    # Also, copy Tomcat version specific files to the target directory
+    switch ($version)
+    {
+        '8.5-jre8'
+        {
+        	copy-item -recurse shared\tomcat\8.5\* $version
+            $content = ((Get-Content -path $dockerFileTemplatePath -Raw) `
+                -replace '__PLACEHOLDER_BASEIMAGE__','mcr.microsoft.com/java/jre-headless:8u202-zulu-alpine') `
+                -replace '__PLACEHOLDER_AI_VERSION__','2.1.2' `
+                -replace '__PLACEHOLDER_TOMCAT_VERSION__','8.5.35' `
+                -replace '__PLACEHOLDER_TOMCAT_MAJOR__','8'
+            break
+        }
+
+        '8.5-java11'
+        {
+        	copy-item -recurse shared\tomcat\8.5\* $version
+            $content = ((Get-Content -path $dockerFileTemplatePath -Raw) `
+                -replace '__PLACEHOLDER_BASEIMAGE__','mcr.microsoft.com/java/jre-headless:11u2-zulu-alpine') `
+                -replace '__PLACEHOLDER_AI_VERSION__','2.1.2' `
+                -replace '__PLACEHOLDER_TOMCAT_VERSION__','8.5.35' `
+                -replace '__PLACEHOLDER_TOMCAT_MAJOR__','8' 
+            break
+        }
+
+        '9.0-jre8'
+        {
+        	copy-item -recurse shared\tomcat\9.0\* $version
+            $content = ((Get-Content -path $dockerFileTemplatePath -Raw) `
+                -replace '__PLACEHOLDER_BASEIMAGE__','mcr.microsoft.com/java/jre-headless:8u202-zulu-alpine') `
+                -replace '__PLACEHOLDER_AI_VERSION__','2.1.2' `
+                -replace '__PLACEHOLDER_TOMCAT_VERSION__','9.0.13' `
+                -replace '__PLACEHOLDER_TOMCAT_MAJOR__','9'
+            break
+        }
+
+        '9.0-java11'
+        {
+        	copy-item -recurse shared\tomcat\9.0\* $version
+            $content = ((Get-Content -path $dockerFileTemplatePath -Raw) `
+                -replace '__PLACEHOLDER_BASEIMAGE__','mcr.microsoft.com/java/jre-headless:11u2-zulu-alpine') `
+                -replace '__PLACEHOLDER_AI_VERSION__','2.1.2' `
+                -replace '__PLACEHOLDER_TOMCAT_VERSION__','9.0.13' `
+                -replace '__PLACEHOLDER_TOMCAT_MAJOR__','9'
+            break
+        }
+    }
+    Set-Content -Value $content -Path $dockerFileOutPath
 }
 
 setup -version '8.5-jre8'
