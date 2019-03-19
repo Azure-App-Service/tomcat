482872bcb469cd11d85dd5e13029325081740cc9
pagariyaalok <alok.pagariya@microsoft.com>
2019-03-18 19:26:52 -0700

    Update setenv.sh
    commnets updated.

--- shared/tomcat/bin/setenv.sh
+++ shared/tomcat/bin/setenv.sh
@@ -1,7 +1,7 @@
 ##### This file will be called from catalina.sh #####
 
 # Starting tomcat 8.5, by default the files created by tomcat doesn’t grant read access to “others”, this includes log files.
-# Override Tomcat's default UMASK of 0027 (rw-r-----) with 0022 (-rw-r--r--) for td-agent on host to read log files (AzMon)
+# Override Tomcat's default UMASK of 0027 (rw-r-----) with 0022 (-rw-r--r--).
 export UMASK="0022"
 
 # Any changes to JUL needs to be loaded at system level. https://tomcat.apache.org/tomcat-9.0-doc/class-loader-howto.html
